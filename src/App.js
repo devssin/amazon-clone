23b4021e529b8e9e773e3909ee2bbdb943b6fb56
louhabAmine <amine.louhab.officiel@gmail.com>
2022-02-08 17:31:48 +0100

    Edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={[<Header />, <Home />]} />
+        </Routes>
+      </Router>
     </div>
   );
 }
